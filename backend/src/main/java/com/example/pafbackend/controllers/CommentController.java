089f1ca1d1eb52692e1149977015da3657bd9ace
SouroN21 <dasunpriyanaveen@gmail.com>
2025-04-26 18:02:20 +0530

    update controll

--- backend/src/main/java/com/example/pafbackend/controllers/CommentController.java
+++ backend/src/main/java/com/example/pafbackend/controllers/CommentController.java
@@ -11,7 +11,6 @@ import com.example.pafbackend.repositories.UserRepository;
 import java.util.List;
 import java.util.Optional;
 
-
 @RestController
 @RequestMapping("/api/comments")
 public class CommentController {
